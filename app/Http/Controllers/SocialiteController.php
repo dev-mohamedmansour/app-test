59d10b8392cca44d05fdffda80176f499d27a7af
mohamed mansour <mohamedmansour0037@gmail.com>
2024-12-27 02:45:56 +0200

    Add GitHub OAuth login
    Adds GitHub OAuth login functionality.  Includes configuration and routes.

---
+++ app/Http/Controllers/SocialiteController.php
@@ -0,0 +1,20 @@
+<?php
+	  
+	  namespace App\Http\Controllers;
+	  
+	  use Laravel\Socialite\Facades\Socialite;
+	  
+	  class SocialiteController extends Controller
+	  {
+			 public function login()
+			 {
+					return Socialite::driver('github')->redirect();
+			 }
+			 
+			 public function redirect()
+			 {
+					$user = Socialite::driver('github')->user();
+					dd($user);
+			 }
+			 
+	  }
